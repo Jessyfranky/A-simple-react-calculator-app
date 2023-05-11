e6640fc8dba36db15fc23fe46e6bb0bdcc8e8ac5
Jessyfranky <115389742+Jessyfranky@users.noreply.github.com>
2023-05-11 13:19:40 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import { Component } from 'react';
+import './index.css';
+import Appview from './views/Appview'
+
+
+
+
+class App extends Component {
+    render(){
+        return (
+            <Appview />
+        )
+    }
+}
+export default App
